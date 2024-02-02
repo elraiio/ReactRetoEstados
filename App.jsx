9b5c370821b79eb6dc387156c6907d7e1cf67849
Diego Pavon <144182189+elraiio@users.noreply.github.com>
2024-02-02 12:04:36 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,31 +1,31 @@
-// App.jsx
-
 import React from 'react';
-import Tarjeta from './Tarjeta'; 
+import ReactDOM from 'react-dom';
 import Tarjeta from './Tarjeta';
 
 const App = () => {
+    const descripcionComun = "Esta es una consola que entretiene a los programadores en sus breaks";
+
     return (
         <div>
             <section>
                 <Tarjeta
-                    imagen="https://pxc.cdn.ellitoral.com.ar/litoral/072013/1515428041087.webp?cw=770&ch=440&extw=jpg"
-                    titulo="Consola Video Juegos"
-                    descripcion="Esta es una consola que entretine a los programadores en sus breaks"
+                    imagen="https://acdn.mitiendanube.com/stores/002/500/835/products/11-1956ed0bdf91c3a3f216070936724610-640-0-74e6bbe0e715b7522c17049945879596-1024-1024.png"
+                    titulo="Consola Video Juegos 1"
+                    descripcion={descripcionComun}
                 />
             </section>
             <section>
                 <Tarjeta
-                    imagen="https://pxc.cdn.ellitoral.com.ar/litoral/072013/1515428041087.webp?cw=770&ch=440&extw=jpg"
-                    titulo="Consola Video Juegos"
-                    descripcion="Esta es una consola que entretine a los programadores en sus breaks"
+                    imagen="https://acdn.mitiendanube.com/stores/002/500/835/products/11-1956ed0bdf91c3a3f216070936724610-640-0-74e6bbe0e715b7522c17049945879596-1024-1024.png"
+                    titulo="Consola Video Juegos 2"
+                    descripcion={descripcionComun}
                 />
             </section>
             <section>
                 <Tarjeta
-                    imagen="https://pxc.cdn.ellitoral.com.ar/litoral/072013/1515428041087.webp?cw=770&ch=440&extw=jpg"
-                    titulo="Consola Video Juegos"
-                    descripcion="Esta es una consola que entretine a los programadores en sus breaks"
+                    imagen="https://acdn.mitiendanube.com/stores/002/500/835/products/11-1956ed0bdf91c3a3f216070936724610-640-0-74e6bbe0e715b7522c17049945879596-1024-1024.png"
+                    titulo="Consola Video Juegos 3"
+                    descripcion={descripcionComun}
                 />
             </section>
         </div>
